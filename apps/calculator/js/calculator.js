d105d9cb71d10ee51fa412194ef4e2d3fe5c2937
MortCodesWeb <52796888+mortada-killer0@users.noreply.github.com>
2024-02-29 15:55:56 +0300

    Calculator overhaul

--- apps/calculator/js/calculator.js
+++ apps/calculator/js/calculator.js
@@ -1,53 +1,108 @@
 !(function (exports) {
   'use strict';
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Get the calculator buttons
-    const buttons = document.getElementsByClassName('button');
+  const Calculator = {
+    displayInput: document.getElementById('display-input'),
+    historyList: document.getElementById('history-list'),
+    buttons: null,
 
-    // Attach onclick event handler to each button
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', buttonClickHandler);
+    inputData: '',
+    historyData: [],
+
+    init: function () {
+      this.historyData = JSON.parse(localStorage.getItem('calculator.history')) || [];
+
+      this.buttons = document.querySelectorAll('input[data-type]');
+      this.buttons.forEach(this.handleEachButton.bind(this));
+
+      this.updateHistoryList();
+    },
+
+    handleEachButton: function (button) {
+      button.addEventListener('click', this.handleButtonClick.bind(this, button));
+    },
+
+    handleButtonClick: function (button, event) {
+      var buttonValue = button.value;
+
+      if (button.dataset.type === 'command') {
+        switch (buttonValue) {
+          case 'C':
+            this.inputData = '';
+            this.displayInput.textContent = this.inputData;
+            break;
+
+          case '=':
+            try {
+              const equation = eval(this.inputData);
+              this.historyData.push({
+                input: this.inputData,
+                equation
+              });
+              localStorage.setItem('calculator.history', JSON.stringify(this.historyData));
+              this.inputData = equation;
+            } catch (error) {
+              this.inputData = 'Error';
+            }
+            Counter.increment(this.displayInput, this.inputData);
+            break;
+
+          default:
+            break;
+        }
+      } else if (button.dataset.type === 'operator') {
+        switch (buttonValue) {
+          case '÷':
+            this.inputData += '/';
+            break;
+
+          case '×':
+            this.inputData += '*';
+            break;
+
+          case '+':
+            this.inputData += '+';
+            break;
+
+          case '-':
+            this.inputData += '-';
+            break;
+
+          default:
+            break;
+        }
+        this.displayInput.textContent = this.inputData;
+      } else {
+        this.inputData += buttonValue;
+        this.displayInput.textContent = this.inputData;
+      }
+
+      this.updateHistoryList();
+    },
+
+    updateHistoryList: function () {
+      this.historyList.innerHTML = '';
+
+      for (let index = 0; index < this.historyData.length; index++) {
+        const item = this.historyData[index];
+
+        const list = document.createElement('ul');
+        this.historyList.appendChild(list);
+
+        const element = document.createElement('li');
+        element.classList.add('noclick');
+        list.appendChild(element);
+
+        const equation = document.createElement('p');
+        equation.textContent = item.equation;
+        element.appendChild(equation);
+
+        const input = document.createElement('p');
+        input.textContent = item.input;
+        element.appendChild(input);
+      }
     }
-  });
+  };
 
-  function buttonClickHandler (event) {
-    const value = event.target.innerHTML;
-
-    switch (value) {
-      case 'C':
-        clearResult();
-        break;
-      case '←':
-        deleteLast();
-        break;
-      case '=':
-        calculate();
-        break;
-      default:
-        appendToResult(value);
-        break;
-    }
-  }
-
-  function appendToResult (value) {
-    document.getElementById('result').value += value;
-  }
-
-  function clearResult () {
-    document.getElementById('result').value = '';
-  }
-
-  function deleteLast () {
-    const result = document.getElementById('result').value;
-    document.getElementById('result').value = result.slice(0, -1);
-  }
-
-  function calculate () {
-    const result = document.getElementById('result').value;
-    const answer = result.replace('÷', '/');
-    document.getElementById('result').value = answer.toLocaleString(
-      L10n.currentLanguage
-    );
-  }
+  Calculator.init();
 })(window);
